5f1975b7665c7540fc2b9815f53457ecbc6120bf
Farid Hajji <farid@hajji.name>
2018-04-15 20:03:14 +0200

    Added IV to GUI.

--- WtTest/main.cpp
+++ WtTest/main.cpp
@@ -28,10 +28,12 @@ private:
 	std::unique_ptr<Crypto> crypto_;
 
 	Wt::WText     *keyText_;
+	Wt::WText     *ivText_;
 	Wt::WTextArea *plainTextEdit_;
 	Wt::WTextArea *cipherTextEdit_;
 
 	void newkey();
+	void newiv();
 	void encrypt();
 	void decrypt();
 };
@@ -50,42 +52,61 @@ EncDecApplication::EncDecApplication(const Wt::WEnvironment& env)
 
 	auto grid = root()->setLayout(std::make_unique<Wt::WGridLayout>());
 
-	grid->addWidget(std::make_unique<Wt::WText>("Key "), 0, 0);
+	grid->addWidget(std::make_unique<Wt::WText>("Key"), 0, 0);
 	keyText_ = grid->addWidget(std::make_unique<Wt::WText>(), 0, 1);
 	auto buttonKey = grid->addWidget(std::make_unique<Wt::WPushButton>("Generate Key"), 0, 2);
 
-	grid->addWidget(std::make_unique<Wt::WText>("Plaintext "), 1, 0);
-	plainTextEdit_ = grid->addWidget(std::make_unique<Wt::WTextArea>(), 1, 1);
-	plainTextEdit_->setFocus();
-	auto buttonEncrypt = grid->addWidget(std::make_unique<Wt::WPushButton>("Encrypt"), 1, 2);
-	
-	grid->addWidget(std::make_unique<Wt::WText>("Ciphertext "), 2, 0);
-	cipherTextEdit_ = grid->addWidget(std::make_unique<Wt::WTextArea>(), 2, 1);
-	auto buttonDecrypt = grid->addWidget(std::make_unique<Wt::WPushButton>("Decrypt"), 2, 2);
+	grid->addWidget(std::make_unique<Wt::WText>("IV"), 1, 0);
+	ivText_ = grid->addWidget(std::make_unique<Wt::WText>(), 1, 1);
+	auto buttonIV = grid->addWidget(std::make_unique<Wt::WPushButton>("Generate IV"), 1, 2);
 
-	grid->setRowStretch(1, 1);
+	grid->addWidget(std::make_unique<Wt::WText>("Plaintext"), 2, 0);
+	plainTextEdit_ = grid->addWidget(std::make_unique<Wt::WTextArea>(), 2, 1);
+	plainTextEdit_->setFocus();
+	auto buttonEncrypt = grid->addWidget(std::make_unique<Wt::WPushButton>("Encrypt"), 2, 2);
+	
+	grid->addWidget(std::make_unique<Wt::WText>("Ciphertext"), 3, 0);
+	cipherTextEdit_ = grid->addWidget(std::make_unique<Wt::WTextArea>(), 3, 1);
+	auto buttonDecrypt = grid->addWidget(std::make_unique<Wt::WPushButton>("Decrypt"), 3, 2);
+
 	grid->setRowStretch(2, 1);
+	grid->setRowStretch(3, 1);
 	grid->setColumnStretch(1, 1);
 
 	buttonKey->clicked().connect(this, &EncDecApplication::newkey);
+	buttonIV->clicked().connect(this, &EncDecApplication::newiv);
 	buttonEncrypt->clicked().connect(this, &EncDecApplication::encrypt);
 	buttonDecrypt->clicked().connect(this, &EncDecApplication::decrypt);
 
 	plainTextEdit_->enterPressed().connect(this, &EncDecApplication::encrypt);
 	cipherTextEdit_->enterPressed().connect(this, &EncDecApplication::decrypt);
+
+	newkey(); // initialize key
+	newiv();  // initialize iv
 }
 
 void EncDecApplication::newkey()
 {
 	// generate a new random key
 	crypto_->newKey(16);
-	crypto_->newIV(16);
 
 	std::ostringstream oss;
 	auto key = crypto_->key();
 	for (std::size_t i = 0; i<key.size(); ++i)
 		oss << std::hex << static_cast<unsigned int>(key[i]);
 	keyText_->setText(oss.str());
+}
+
+void EncDecApplication::newiv()
+{
+	// generate a new random IV
+	crypto_->newIV(16);
+
+	std::ostringstream oss;
+	auto iv = crypto_->iv();
+	for (std::size_t i = 0; i<iv.size(); ++i)
+		oss << std::hex << static_cast<unsigned int>(iv[i]);
+	ivText_->setText(oss.str());
 }
 
 void EncDecApplication::encrypt()
